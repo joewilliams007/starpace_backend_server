03483c44f1bdee152f485f0f11ab1f5e69df6d81
Universe <williamsthehack@gmail.com>
2023-01-30 12:40:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const longTimeRegistartionLimiter = rateLimit({
 
 const voteLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 10, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
+	max: 15, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
